3978cbf1211393da83c54fd6472771dc629dd08e
xxxTOFxxx <Rapadura01@>
2023-12-13 03:00:05 +0000

    addition of calculation base

---
+++ src/simplifier/TaxClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simplifier;
+
+/**
+ *
+ * @author Tiago
+ */
+public class TaxClass {
+    // Outros atributos...
+
+    private int userID;
+
+    //public TaxClass(/* Outros parâmetros */, int userID) {
+        // Inicialize outros atributos...
+    //    this.userID = userID;
+    }
+
+    
+
